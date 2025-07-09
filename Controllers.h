0dcf2f985448ffba7fa56be24a8a8369bd4419de
EssJimmy <jperez81@itam.mx>
2025-07-09 08:19:29 -0600

    finished development for the controller section, now the three controllers work, adaptive controller only needs to be modified in the gains

--- Controllers.h
+++ Controllers.h
@@ -11,9 +11,6 @@ public:
     static std::vector<std::vector<double>> parra_vega_controller(const double pi, const double sample_time, bool i_c_smc,
         static double ti, const double *qm);
 
-    static std::vector<std::vector<double>> nl_controller(const double pi, const double sample_time, bool i_c_smc,
-        double *qm, static double ti);
-
     static std::vector<std::vector<double>> adaptive_controller(const double pi, const double sample_time, const bool i_c_smc, double* qm, static double ti);
 
     static std::vector<double> graph_trajectory(const double t, const double pi, const double* qm, const double* dot_delta_pos, 
